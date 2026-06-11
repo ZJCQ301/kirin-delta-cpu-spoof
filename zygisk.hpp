d19409436a744017792f20462ac9ed4affaa614e
ZJCQ301 <125265493+ZJCQ301@users.noreply.github.com>
2026-06-12 02:20:24 +0800

    Create zygisk.hpp

---
+++ zygisk.hpp
@@ -0,0 +1,50 @@
+#ifndef ZYGISK_HPP
+#define ZYGISK_HPP
+
+#include <jni.h>
+#include <string>
+
+namespace zygisk {
+
+struct AppSpecializeArgs {
+    JNIEnv* env;
+    jobject nice_name;
+    jstring app_data_dir;
+    const char* const* rlimits;
+    void (*onExit)();   // 进程退出回调
+};
+
+struct ServerSpecializeArgs {
+    JNIEnv* env;
+    jstring nice_name;
+};
+
+class Api {
+public:
+    virtual ~Api() = default;
+    virtual const char* getProcessName() const = 0;
+    virtual void pltHookRegister(const char* regex, const char* symbol, void* newFunc, void** oldFunc) = 0;
+    // 其他必要方法（根据需求可扩展）
+};
+
+class ModuleBase {
+public:
+    virtual ~ModuleBase() = default;
+    virtual void onLoad(Api* api, JNIEnv* env) {}
+    virtual void preAppSpecialize(AppSpecializeArgs* args) {}
+    virtual void postAppSpecialize(const AppSpecializeArgs* args) {}
+    virtual void preServerSpecialize(ServerSpecializeArgs* args) {}
+    virtual void postServerSpecialize(const ServerSpecializeArgs* args) {}
+};
+
+#define REGISTER_ZYGISK_MODULE(className) \
+    extern "C" __attribute__((visibility("default"))) \
+    zygisk::ModuleBase* zygiskModuleOnLoad(zygisk::Api* api, JNIEnv* env) { \
+        auto* mod = new className(); \
+        mod->onLoad(api, env); \
+        return mod; \
+    }
+
+} // namespace zygisk
+
+#endif
